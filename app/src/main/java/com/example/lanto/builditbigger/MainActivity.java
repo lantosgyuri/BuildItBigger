bead1bff6e3e476b083bf8f24e3ebbfe384707c4
lantosgyuri <lantos.gyorgy@gmail.com>
2018-07-16 10:18:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lanto/builditbigger/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lanto.builditbigger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.main_fragment_container, new MainFragment())
+                .commit();
+    }
+}
